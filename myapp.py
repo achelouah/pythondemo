006b5cfa23991a498bff9705fc3204afaac66332
Abdelkader Chelouah <a.chelouah@gmail.com>
2022-06-29 10:48:47 +0200

    Set logfile, result and wait from environment variables

--- myapp.py
+++ myapp.py
@@ -1,17 +1,34 @@
 import logging
 import time
+import os
 
-logging.basicConfig(format='%(asctime)s %(message)s', filename='message.log', encoding='utf-8', level=logging.DEBUG)
+logfile=os.environ.get('LOGFILE')
+result=os.environ.get('RESULT')
+wait=os.environ.get('WAIT')
+
+if logfile is not None:
+    logging.basicConfig(format='%(asctime)s %(message)s', filename=logfile, encoding='utf-8', level=logging.DEBUG)
+else:
+    print(f'Environment variable LOGFILE is not set')
+    logging.basicConfig(format='%(asctime)s %(message)s', filename='python.log', encoding='utf-8', level=logging.DEBUG)
+
+if wait is not None:
+    w=int(wait)
+else:
+    w=30
 
 i=1
-while i <=30:
-    k=31-i
+while i <=w:
+    k=w+1-i
     print('{0:2d} sec remaining ...'.format(k))
     logging.info('Waiting for %s sec', k)
     time.sleep(1)
     i+=1
 
-with open('readme.txt', 'w') as f:
-    f.write('Create a new text file!')
+if result is not None:
+    with open(result, 'w') as f:
+        f.write('Create a new text file!')
+else:
+    print(f'Environment variable RESULT is not set')
 
 
